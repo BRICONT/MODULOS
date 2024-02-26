002943527be2e67051f23c93cab517ad4c669057
BRICONT <bjulian1605@gmail.com>
2024-02-26 06:40:22 -0500

    realizo la validacion eh insercion de datos para las tablas de pension y salud

--- conexion/db.php
+++ conexion/db.php
@@ -1,14 +1,14 @@
 <?php
-// Conexión a la base de datos
-$servername = "localhost"; // Cambiar si es necesario
-$username = "root"; // Cambiar por tu usuario de MySQL
-$password = ""; // Cambiar por tu contraseña de MySQL
-$dbname = "n_algj"; // Cambiar por el nombre de tu base de datos
 
-// Crear conexión
+$servername = "localhost"; 
+$username = "root"; 
+$password = ""; 
+$dbname = "n_algj"; 
+
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar la conexión
+
 if ($conn->connect_error) {
     die("La conexión falló: " . $conn->connect_error);
 }
